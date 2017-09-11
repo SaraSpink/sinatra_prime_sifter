bd7ac44f642bd797b7ea5d76c1b09a942b41ea78
Gabrijela Gavric and Sara Spink <sara@>
2017-09-11 14:06:18 -0700

    fix rspec to match lib written

--- spec/sinatra_prime_spec.rb
+++ spec/sinatra_prime_spec.rb
@@ -1,15 +1,20 @@
 require('rspec')
 require('sinatra_prime')
 
-describe('Stuff#check') do
+describe('Prime#check') do
   it("Returns true if number is prime") do
     list = Prime.new(5)
-    expect(list.check).to eq(true)
+    expect(list.check?).to eq(true)
   end
 
-  it("returns true if you use the attribute variable") do
-    number = Prime.new(@number)
-    expect(list.check).to eq(true)
+  it("returns a boolean if you use the attribute variable") do
+    number = Prime.new(5)
+    expect(number.check?).to eq(true)
+  end
+
+  it("returns a boolean if you use the attribute variable") do
+    number = Prime.new(6)
+    expect(number.check?).to eq(false)
   end
 
 
